f53ec5e751420ceb4c6abbae290d807fdf2a71bd
Beata <manska.b@gmail.com>
2025-02-06 19:09:14 +0100

    langv

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -6,11 +6,11 @@ import 'bootstrap/dist/css/bootstrap.css'
 import { createI18n } from 'vue-i18n';
 
 
-function locadLocaleMessages() {
+function loadLocaleMessages() {
 	const locales = require.context('./assets/i18n', true, /[A-Za-z0-9-_,\s]+\.json$/i);
 	const messages = {};
 	locales.keys().forEach(key => {
-		const matched = key.match(/[A-Za-z0-9-_]+)\./i);
+		const matched = key.match(/([A-Za-z0-9-_]+)\./i);
 		if (matched && matched.length > 1) {
 			const locale = matched[1];
 			messages[locale] = locales(key).default;
@@ -19,23 +19,24 @@ function locadLocaleMessages() {
 	return messages;
 }
 
-const messages = {
-	en: {
-		message: {
-			hello: 'Hello World'
-		}
-	},
-	pl: {
-		message: {
-			hello: 'Cześć'
-		}
-	}
-};
+// const messages = {
+// 	en: {
+// 	navbar: {
+// 	about: 'About us'
+// 	}
+// },
+// 	pl: {
+// 	navbar: {
+// 	about: 'O nas'
+// 	}
+// }
+// };
 
 const i18n = createI18n({
 	locale: 'en',
 	fallbackLocale: 'en',
-	messages: locadLocaleMessages,
+	messages: loadLocaleMessages()
+	// messages,
 });
 
 
